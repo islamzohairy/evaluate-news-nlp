7a7037497646215819d5c4f32845541e4ad44a6a
Islam Elzohairy <zohairy93@gmail.com>
2021-04-16 03:37:56 +0200

    Make back end req

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -9,62 +9,44 @@ function handleSubmit(event) {
 
   if (checkURL) {
     console.log("::: Form Submitted :::");
-    // fetch("http://localhost:8081/test")
-    fetch("https://evaluate-news-nlp-wepback.herokuapp.com/test")
-      .then((res) => res.json())
-      .then(async function (res) {
-        console.log(res.key);
-        const formdata = new FormData();
-        formdata.append("key", res.key);
-        formdata.append("url", formText);
-        formdata.append("lang", res.lang);
-
         const requestOptions = {
           method: "POST",
-          body: formdata,
-          redirect: "follow",
+          body: {
+            formText
+          },
+          headers: {
+            'Content-Type': 'application/json'
+          }
         };
 
-        const response = await fetch(
-          "https://api.meaningcloud.com/sentiment-2.1",
+        await fetch(
+          "https://evaluate-news-nlp-wepback.herokuapp.com/test",
           requestOptions
         )
-          .then(async (response) => ({
-            status: response.status,
-            body: await response.json(),
-          }))
-          .then(({ status, body }) => {
-            console.log("status: ", status);
-            console.log("body: ", body);
-            return {
-              status,
-              body,
-            };
-          })
-          .catch((error) => console.log("error", error));
-
-        if (response.body.status.code !== "0") {
-          document.getElementById("loading").innerHTML =
-            response.body.status.msg + " " + "🙁";
-          document.getElementById("confidence").innerHTML = "";
-          document.getElementById("polarity").innerHTML = "";
-        } else {
-          if (response.body.score_tag) {
-            let polarity = await Client.checkPolarity(response.body.score_tag);
-            document.getElementById("polarity").innerHTML = polarity;
+        .then((response) => {
+          console.log(response)
+          if (response.body.status.code !== "0") {
+            document.getElementById("loading").innerHTML =
+              response.body.status.msg + " " + "🙁";
+            document.getElementById("confidence").innerHTML = "";
+            document.getElementById("polarity").innerHTML = "";
+          } else {
+            if (response.body.score_tag) {
+              let polarity = await Client.checkPolarity(response.body.score_tag);
+              document.getElementById("polarity").innerHTML = polarity;
+            }
+  
+            if (response.body.confidence) {
+              let { confidence } = response.body;
+              document.getElementById("confidence").innerHTML = confidence;
+            }
+  
+            document.getElementById("loading").innerHTML = "😀";
+  
+            // document.getElementById("polarity").innerHTML = "polarity";
+            // document.getElementById("confidence").innerHTML = "confidence";
           }
-
-          if (response.body.confidence) {
-            let { confidence } = response.body;
-            document.getElementById("confidence").innerHTML = confidence;
-          }
-
-          document.getElementById("loading").innerHTML = "😀";
-
-          // document.getElementById("polarity").innerHTML = "polarity";
-          // document.getElementById("confidence").innerHTML = "confidence";
-        }
-      })
+        })
       .catch((e) => {
         console.log(e);
         document.getElementById("loading").innerHTML =
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,12 @@
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
+const request = require("request");
 
 const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.static("dist"));
 
@@ -19,6 +23,23 @@ app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
+app.get("/test", async function (req, res) {
+  const formdata = new FormData();
+  formdata.append("key", process.env.API_KEY);
+  formdata.append("url", req.body.formText);
+  formdata.append("lang", "auto");
+
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+    uri: "https://api.meaningcloud.com/sentiment-2.1",
+  };
+  const result = await request(requestOptions, function (error, response) {
+    // console.log("response: ", response);
+    console.log(error, response.body);
+    return response.body;
+  });
+  res.json(result);
+  // res.send(mockAPIResponse);
 });
